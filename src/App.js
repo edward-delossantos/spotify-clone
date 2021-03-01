d479758a8cb3e09a9dd95fc6de2a263fdcac83f9
edward-delossantos <35975985+edward-delossantos@users.noreply.github.com>
2021-03-01 16:42:23 -0500

    tutorial completed

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import './App.css';
 import Login from './Login';
 import { getTokenFromUrl } from "./spotify";
 import SpotifyWebApi from "spotify-web-api-js";
 import Player from "./Player";
+import { useDataLayerValue } from "./DataLayer";
 
-const spotify = new SpotifyWebApi(); //create an instance of spotify in the app
+//create an instance of spotify in the app
+const spotify = new SpotifyWebApi(); 
 
 function App() {
-  const [token, setToken] = useState(null);
+const [{ user, token }, dispatch] = useDataLayerValue();
 
   // Run code based on given condition 
   useEffect(() => {
@@ -17,21 +19,42 @@ function App() {
     const _token = hash.access_token;
 
     if (_token) {
-      setToken(_token);
-    
+      dispatch({
+        type: "SET_TOKEN",
+        token: _token,
+      })
+      
       spotify.setAccessToken(_token);
 
       spotify.getMe().then(user => {
-        console.log(":person:", user)
-      })      //Promise to get users account
+        dispatch({
+          type: 'SET_USER',
+          user: user,
+        });
+      });      //Promise to get users account
+
+      spotify.getUserPlaylists().then((playlists) => {
+        dispatch ({
+          type: "SET_PLAYLISTS",
+          playlists: playlists
+        })
+      })
+
+      spotify.getPlaylist("37i9dQZEVXcV2lpvXAI2Ll").then(response => 
+        dispatch({
+          type: "SET_DISCOVER_WEEKLY",
+          discover_weekly: response
+        })
+      )
     }
   }, []);
 
   return (
     <div className="app">
       {
+        //verify token before going into the player
         token ? (
-          <Player /> //verify token before going into the player
+          <Player spotify={spotify} />
         ) : (
           <Login />
         )
